ab0bd972b365fa65a269661cfc190a8cc355f59a
Jesus Barba <48895432+saintjeus@users.noreply.github.com>
2020-12-12 17:34:10 -0700

    Add files via upload

---
+++ client.c
@@ -0,0 +1,66 @@
+
+// Client side implementation of UDP client-server model 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <netinet/in.h> 
+
+#include <stdlib.h>
+#include <stdio.h>
+  
+#define PORT     8080 
+#define MAXLINE 1024 
+  
+// Driver code 
+int main() { 
+    int sockfd; 
+    char buffer[MAXLINE]; 
+    char *hello = "Hello from client";
+    char *buffer2[MAXLINE]; //second buffer, size set to 1024 
+    struct sockaddr_in     servaddr; 
+  
+    // Creating socket file descriptor 
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+        perror("socket creation failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+  
+    memset(&servaddr, 0, sizeof(servaddr)); 
+      
+    // Filling server information 
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_port = htons(PORT); 
+    servaddr.sin_addr.s_addr = INADDR_ANY; 
+      
+    int n, len; 
+    
+    
+    // loop here to get input, send to server, and receive from server
+    while(1)
+    {
+    printf("\nClient: :");
+    gets(buffer2);  
+    sendto(sockfd, (char *)buffer2, strlen(buffer2), 
+        MSG_CONFIRM, (const struct sockaddr *) &servaddr,  
+            sizeof(servaddr)); 
+    printf("Message sent.\n"); 
+    
+    // break loop if user wants to end session with "end"
+    if(strcmp(buffer2,"end")==0)
+    	break;
+    	
+    
+    n = recvfrom(sockfd, (char *)buffer, MAXLINE,  
+                MSG_WAITALL, (struct sockaddr *) &servaddr, 
+                &len); 
+    buffer[n] = '\0'; 
+    printf("Server : %s\n", buffer); 
+  
+    close(sockfd); 
+    }
+    return 0; 
+} 
